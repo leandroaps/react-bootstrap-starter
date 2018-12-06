924c69364cd2d29707b8723627ba44a1721c0b84
Leandro A. Siqueira <leandroaps@gmail.com>
2018-12-06 15:11:33 -0200

    Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "bootstrap";
-import "./starter-template.css";
+import "./Assets/Styles/starter-template.css";
 
 import React, { Component } from "react";
 
-import Main from "./Main";
-import Nav from "./Nav";
+import Main from "./Components/Main/";
+import Nav from "./Components/Nav";
 
 class App extends Component {
   render() {
